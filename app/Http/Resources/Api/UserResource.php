7649bb9d4b9bc4bf5f879675293bc05348206cdf
ShibaPipi <906262260@qq.com>
2019-09-07 23:02:16 +0800

    枚举引入

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api;
 
+use App\Models\Enum\UserEnum;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
@@ -14,23 +15,10 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        switch ($this->status) {
-            case -1:
-                $this->status = '已删除';
-                break;
-            case 0:
-                $this->status = '正常';
-                break;
-            case 1:
-                $this->status = '冻结';
-                break;
-        }
-
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'email' => $this->email,
-            'status' => $this->status,
+            'status' => UserEnum::getStatusName($this->status),
             'created_at' => (string)$this->created_at,
             'updated_at' => (string)$this->updated_at
         ];
