dc3e20309f00cc6623d8cb774937a5c98ac12e50
ShibaPipi <906262260@qq.com>
2019-09-27 08:12:34 +0800

    优化注册

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -2,50 +2,25 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
-
 class UserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         switch ($this->method()) {
             case 'GET':
-                {
-                    return [
-                        'id' => ['required,exists:shop_user,id']
-                    ];
-                }
+                return [
+                    'id' => ['required,exists:shop_user,id']
+                ];
             case 'POST':
-                {
-                    return [
-                        'name' => ['required', 'max:12', 'unique:users,name'],
-                        'password' => ['required', 'max:16', 'min:6']
-                    ];
-                }
+                return [
+                    'name' => ['required', 'max:12', 'unique:users,name'],
+                    'password' => ['required', 'max:16', 'min:6']
+                ];
             case 'PUT':
             case 'PATCH':
             case 'DELETE':
             default:
-                {
-                    return [
-
-                    ];
-                }
+                return [];
         }
     }
 
